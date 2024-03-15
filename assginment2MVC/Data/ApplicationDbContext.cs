558643194c468fdb162b3584d611e28f97ee7a8c
chino0522 <baekjinho0522@gmail.com>
2024-03-14 21:16:22 -0700

    first commit

---
+++ assginment2MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace assginment2MVC.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
